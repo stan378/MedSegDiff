3a5f41220b0a0f9d653a0232020a4bb8f3b67d8f
maxization <damian.opoka99@gmail.com>
2023-06-28 14:31:57 +0200

    Add logs

--- scripts/segmentation_train.py
+++ scripts/segmentation_train.py
@@ -42,6 +42,7 @@ def main():
         ds = BRATSDataset3D(args.data_dir, transform_train, test_flag=False)
         args.in_ch = 5
     elif args.data_name == 'MRI':
+        print(f"Transformation: {args.trans_type}")
         if args.trans_type == "gray":
             args.in_ch = 2
             tran_list = [transforms.Resize((args.image_size, args.image_size)), transforms.Grayscale(), transforms.ToTensor(),]
@@ -49,9 +50,12 @@ def main():
             args.in_ch = 4
             tran_list = [transforms.Resize((args.image_size, args.image_size)), transforms.Lambda(lambda x: ndi.gaussian_filter(x, sigma=1)),
                          transforms.ToTensor(), ]
-        else: #args.trans_type == "orginal":
+        elif args.trans_type == "orginal":
             args.in_ch = 4
             tran_list = [transforms.Resize((args.image_size, args.image_size)), transforms.ToTensor(), ]
+        else:
+            print("No transformation selected")
+            exit()
         transform_train = transforms.Compose(tran_list)
 
         ds = MRIDataset(args, args.data_dir, transform_train)
